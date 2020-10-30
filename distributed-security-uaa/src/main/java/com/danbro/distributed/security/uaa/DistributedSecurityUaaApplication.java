a819f97af475d99d958a08f12139e9c181827d5d
danbro <710170342@qq.com>
2020-10-30 20:54:40 +0800

    first commit

---
+++ distributed-security-uaa/src/main/java/com/danbro/distributed/security/uaa/DistributedSecurityUaaApplication.java
@@ -0,0 +1,13 @@
+package com.danbro.distributed.security.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSecurityUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSecurityUaaApplication.class, args);
+	}
+
+}
